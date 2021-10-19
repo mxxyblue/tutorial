0f2d5dff9eef9dc871a472ea244a0109fc330cab
jiyeonss <50973174+jiyeonss@users.noreply.github.com>
2021-10-19 14:32:52 +0900

    ㅡㅡ

--- tutorial_pro/src/main/java/com/hk/controller/AController.java
+++ tutorial_pro/src/main/java/com/hk/controller/AController.java
@@ -60,6 +60,42 @@ public class AController extends HttpServlet {
       if(command==null) {
          MultipartRequest multi=null;         
 
+<<<<<<< HEAD
+			String saveDirectory="C:/Academy/eclipse-workspace/tutorial_test/src/main/webapp/upload";
+			
+			//1024byte --> 1kbyte --> 1024kb --> 1MB
+			int maxPostSize=1024*1024*10;
+			
+			try {
+				multi=new MultipartRequest(request, saveDirectory, maxPostSize, "utf-8",
+						                    new DefaultFileRenamePolicy());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			String id=multi.getParameter("id");
+			String title=multi.getParameter("title");
+			String content=multi.getParameter("content");
+			
+			boolean isS=dao.insertBoard(new ADto(id,title,content));
+			
+			String origin_fname=multi.getOriginalFileName("filename");//���ε��Ҷ� �������ϸ��ϱ�
+			
+			//String random32= UUID.randomUUID().toString().replaceAll("-", "");//"-"�����ϰ� 32�ڸ� ������
+			String stored_fname=origin_fname.substring(origin_fname.lastIndexOf("."));
+			                                          // "123.jpg".substring(3) --> ".jpg"
+			
+			int file_size=(int)multi.getFile("filename").length();// file.length() ��ȯŸ�� long
+			
+			boolean fisS=fdao.insertFileInfo(
+					new FileDto(origin_fname,file_size));
+			
+			File oldFile=new File(saveDirectory+"/"+multi.getFilesystemName("filename"));
+			File newFile=new File(saveDirectory+"/"+stored_fname);
+			oldFile.renameTo(newFile);//old--> new�� ���ϸ� �ٲ�
+			
+=======
          String saveDirectory="C:/Users/HYO LYN KIM/eclipse-workspace/tutorial_test"
                + "/src/main/webapp/upload";
          
@@ -95,6 +131,7 @@ public class AController extends HttpServlet {
          File newFile=new File(saveDirectory+"/"+stored_fname);
          oldFile.renameTo(newFile);//old--> new      ϸ   ٲ 
          
+>>>>>>> branch 'main' of https://github.com/mxxyblue/tutorial.git
 
          if(isS) {
             response.sendRedirect("AController.do?command=boardlist");
@@ -175,6 +212,114 @@ public class AController extends HttpServlet {
          }
          //------  ȸ    ø    Ҷ  Ȯ    ڵ      ---------//
 
+<<<<<<< HEAD
+			
+//			List<FileDto> list=fdao.getFileList();//file ��� ����
+//			request.setAttribute("list", list);
+//			request.getRequestDispatcher("Adetailboard.jsp").forward(request, response);
+			
+			request.setAttribute("dto", dto);
+			dispatch("Adetailboard.jsp", request, response);
+		}else if(command.equals("download")) {//���� �ٿ�ε��ϱ�
+			//�ٿ�ε� ��û ���� ������ DB���� ��������
+			int seq=Integer.parseInt(request.getParameter("aseq"));
+			FileDto dto=fdao.getFileInfo(seq);
+			
+			//���� ���� ���
+			String saveDirectory="C:/Academy/eclipse-workspace/tutorial_test/src/main/webapp/upload";
+			
+			String filePath=saveDirectory+"/"+dto.getAorigin_fname();
+			
+			File file=new File(filePath);//File��ü ����
+			
+//			int [] i=new int[3];//[0,0,0]
+//			int []ii={1,2,3,4,5};
+			//java�� �ѹ��� ���� �� �ִ� ���� ũ�⸸ŭ �迭�� ����
+			byte[] b=new byte[(int)file.length()];
+			
+			//�������� ������ �� ������ �ʱ�ȭ
+			response.reset();
+			
+			//�ٿ�ε��ϴ� ������ ������ �𸥴ٸ� octet-stream �����Ѵ�.
+			//��) application/msword
+			response.setContentType("application/octet-stream");
+			
+			//�ѱ����ڵ� : �ѱ����Ͽ� ��� �̸��� ������ ���� ����
+			String encoding=new String(dto.getAorigin_fname().getBytes("utf-8"),"8859_1");
+			
+			//������ �ٿ�ε� ��ư�� Ŭ������ �� �ٿ�ε� ����ȭ���� �������� ó��
+			//���ϸ��� �������ϸ����� �ٲ��ִ� �ڵ�
+			response.setHeader("Content-Disposition", "attachment; filename="+encoding);
+			
+			FileInputStream in =null;//������ �о���̱� ���� ��ü(�Է�)
+			ServletOutputStream out=null;//�������� ���� ��ü(���)
+			
+			try {
+				//file�� �о���̱� ���� �������� �غ��Ѵ�.
+				in=new FileInputStream(file);
+				
+				//����� ���� �������� �غ��Ѵ�.
+				out=response.getOutputStream();
+				
+				//�о���̴� ���� ������ ����
+				int numRead=0;
+				//read()�� ���� ���� �о����
+				while((numRead=in.read(b, 0, b.length))!=-1) {
+					//write()�� ���� ���� ����ϱ�
+					out.write(b, 0, numRead);
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				out.flush();//���� �����Ͱ� ������ ��� �� �о�� ��������
+				out.close();//out��ü �ݱ�
+				in.close();//in��ü �ݱ�
+			}
+		}else if(command.equals("replyboard")) {
+			int seq=Integer.parseInt(request.getParameter("seq"));
+			String id=request.getParameter("id");
+			String title=request.getParameter("title");
+			String content=request.getParameter("content");
+			
+			boolean isS=dao.replyBoard(new ADto(seq,id,title,content));
+			if(isS) {
+				response.sendRedirect("AController.do?command=boardlist");
+			}else {
+				response.sendRedirect("error.jsp?msg="+(URLEncoder.encode("답글추가실패", "utf-8")));
+			}
+		}else if(command.equals("updateform")) {
+			int seq=Integer.parseInt(request.getParameter("seq"));
+			ADto dto = dao.getABoard(seq);
+			request.setAttribute("dto", dto);
+			dispatch("Aupdateboard.jsp",request,response);
+		}else if(command.equals("updateboard")) {
+			int seq=Integer.parseInt(request.getParameter("seq"));
+			String title=request.getParameter("title");
+			String content=request.getParameter("content");
+			
+			boolean isS=dao.updateBoard(new ADto(seq,title,content));
+			
+			if(isS) {
+				//Adetailboard.jsp:응답할 페이지를 먼저 확인해본다
+				//내가 수정한 글을 바로 조회해서 본다.
+				response.sendRedirect("AController.do?command=detailboard&seq="+seq);
+			}else {
+				response.sendRedirect("error.jsp?msg="+URLEncoder.encode("수정실패","utf-8"));
+			}
+		}else if(command.equals("contentAjax")) {
+			int seq=Integer.parseInt(request.getParameter("seq"));
+			
+			ADto dto = dao.getABoard(seq);	//seq에 대한 글상세 내용 조회 --> dto 담음
+			//date타입은 json으로 변환 안됨
+//			dto.setRegdate(null);//regdate만 값을 다시 정의
+			Date regdate = dto.getAregdate();
+			SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd");
+			String regdateStr=sf.format(regdate);//date타입-->String으로 변환
+			dto.setAregdateStr(regdateStr);
+			dto.setAregdateStr(regdateStr);
+			dto.setAregdate(null);
+=======
          
 //         List<FileDto> list=fdao.getFileList();//file         
 //         request.setAttribute("list", list);
@@ -288,6 +433,7 @@ public class AController extends HttpServlet {
          dto.setAregdateStr(regdateStr);
          dto.setAregdateStr(regdateStr);
          dto.setAregdate(null);
+>>>>>>> branch 'main' of https://github.com/mxxyblue/tutorial.git
 //-----------간단한 값 하나 응답할 때 text 타입 예시
          //요청한 페이지 응답하는 코드
          PrintWriter pw=response.getWriter();
